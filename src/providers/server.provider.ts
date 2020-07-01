67ed7f07a806d8e4686717e72ebb2238eb1e9e63
Victor Morales <vmorales@anzen.com.mx>
2020-07-01 11:29:51 -0500

    feat: socket server added

--- src/providers/server.provider.ts
+++ src/providers/server.provider.ts
@@ -1,16 +1,32 @@
 import express from 'express';
 import { SERVER_PORT } from '../global/environment';
 
+import socketIO from 'socket.io';
+import http from 'http';
+
 
 export default class ServerProvider{
     public app: express.Application;
     public port: number;
+
+    public io: socketIO.Server;
+    private httpServer: http.Server;
+
     constructor(){
         this.app = express();
         this.port = SERVER_PORT;
+        this.httpServer = new http.Server( this.app );
+        this.io = socketIO( this.httpServer );
     }
 
-    start( callback:FunctionStringCallback ){
-        this.app.listen( this.port, callback );
+    private listenSockets(){
+        console.log('Escuchando sockets');
+        this.io.on('connection', cliente => {
+            console.log('Cliente conectado');
+        });
+    }
+
+    start( callback:FunctionConstructor ){
+        this.httpServer.listen( this.port, callback );
     }
 }
