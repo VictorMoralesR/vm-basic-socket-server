b82690f7aa3bd3baa4f705edb3a335946b214fd1
Victor Morales <vmorales@anzen.com.mx>
2020-07-01 10:26:32 -0500

    feat: first commit

---
+++ index.ts
@@ -0,0 +1,20 @@
+import ServerProvider from "./src/providers/server.provider";
+import router from './src/app/routes';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+const server = new ServerProvider();
+// body parser 
+server.app.use( bodyParser.urlencoded( { extended: true } ) );
+server.app.use( bodyParser.json() );
+
+// cors
+server.app.use( cors( { origin:true, credentials: true } ) );
+
+// routes
+server.app.use('/', router);
+
+
+server.start(() => {
+    console.log(`El servidor esta corriendo en el puerto ${ server.port }`);
+});
