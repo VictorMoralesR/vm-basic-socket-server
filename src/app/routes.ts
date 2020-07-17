13ffaee5a2c85402de6c67438ce5630e4d9f5ba7
Victor Morales <vmorales@anzen.com.mx>
2020-07-17 11:03:58 -0500

    feat: private message added

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,22 +1,47 @@
 import { Router, Request, Response } from 'express';
+import ServerProvider from '../providers/server.provider';
 
 const router = Router();
 
 
-router.get('/mensajes',(request: Request, response: Response)=>{
+router.get('/messages',(request: Request, response: Response)=>{
     response.json({
         ok: true,
         mensaje: 'Todo bien'
     });
 });
-router.post('/mensajes/:id',(request: Request, response: Response)=>{
-    const cuerpo = request.body.cuerpo;
-    const de = request.body.de;
-    const id = request.params.id;
+router.post('/messages',(request: Request, response: Response)=>{
+    const message = request.body.message;
+    const by = request.body.by;
+    const server = ServerProvider.instance;
+    const payload = {
+        by ,
+        message
+    };
+    server.io.emit('new-private-message',payload);
+
     response.json({
         ok: true,
-        cuerpo,
-        de,
+        message,
+        by,
+    });
+});
+router.post('/messages/:id',(request: Request, response: Response)=>{
+    console.log('message post', request);
+    
+    const message = request.body.message;
+    const by = request.body.by;
+    const id = request.params.id;
+    const server = ServerProvider.instance;
+    const payload = {
+        by ,
+        message
+    };
+    server.io.in( id ).emit('new-private-message',payload);
+    response.json({
+        ok: true,
+        message,
+        by,
         id
     });
 });
